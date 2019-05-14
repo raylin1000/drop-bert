c2115c9e9eb96ef3201e1c2dc4641f9cd0cb368d
Raymond Lin <raylin1000@gmail.com>
2019-05-14 04:40:51 +0000

    Reorganize

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# drop-nmn
+# drop-bert
+
+This repository contains the code for our CS 287 spring 2019 final project.
--- drop_bert/nhelpers.py
+++ drop_bert/nhelpers.py
@@ -1,99 +1,9 @@
-from namedtensor import NamedTensor, ntorch
 import torch
 import numpy as np
 import string
 from word2number.w2n import word_to_num
 from allennlp.nn.util import replace_masked_values
 from itertools import permutations
-
-# def repeat(tensor, dims_sizes):
-#     names = tensor._schema._names
-#     shape = tensor.shape
-
-#     new_sizes = []
-#     for name in names:
-#         if name in dims_sizes:
-#             new_sizes.append(dims_sizes[name])de
-#         else:
-#             new_sizes.append(1)
-#     return NamedTensor(
-#         tensor._tensor.repeat(new_sizes), names
-#     )
-
-# def masked_fill(tensor, mask, value):
-#     order = mask._mask_broadcast_order(tensor._schema._names)
-#     mask = mask._force_order(order)
-#     tensor = tensor._force_order(order)
-#     return NamedTensor(tensor.values.masked_fill(mask.values, value), tensor._schema._names)
-
-# def replace_masked_values(tensor, mask, replace_with):
-#     if tensor.dim() != mask.dim():
-#         raise ConfigurationError(
-#             "tensor.dim() (%d) != mask.dim() (%d)"
-#             % (tensor.dim(), mask.dim())
-#         )
-#     return masked_fill(tensor, (1 - mask).byte(), replace_with)
-
-# def masked_softmax(
-#     vector,
-#     mask,
-#     dim,
-#     memory_efficient=True,
-#     mask_fill_value=-1e32,
-# ):
-#     mask = mask.float()
-#     if not memory_efficient:
-#         result = (vector * mask).softmax(dim)
-#         result = result * mask
-#         result = result / (result.sum(dim) + 1e-13)
-#     else:
-#         masked_vector = masked_fill(vector, (1 - mask).byte(), mask_fill_value)
-#         result = masked_vector.softmax(dim)
-#     return result
-
-
-# def masked_log_softmax(vector, mask, dim):
-#     mask = mask.float()
-#     vector = vector + (mask + 1e-45).log()
-#     return vector.log_softmax(dim)
-
-
-# def get_best_span(span_start_logits, span_end_logits):
-#     batch_size = span_start_logits.shape["batch"]
-#     passage_length = span_start_logits.shape["seqlen"]
-
-#     span_start_logits = span_start_logits.rename(
-#         "seqlen", "sseqlen"
-#     )
-#     span_end_logits = span_end_logits.rename(
-#         "seqlen", "eseqlen"
-#     )
-
-#     span_log_probs = span_start_logits + span_end_logits
-#     device = span_log_probs.values.device
-#     span_log_mask = (
-#         ntorch.triu(
-#             ntorch.ones(
-#                 passage_length,
-#                 passage_length,
-#                 names=("sseqlen", "eseqlen"),
-#             ).to(device),
-#             dims=("sseqlen", "eseqlen"),
-#         )
-#         .log()
-#     )
-#     valid_span_log_probs = span_log_probs + span_log_mask
-
-#     best_spans = valid_span_log_probs.stack(
-#         ("sseqlen", "eseqlen"), "seqlen"
-#     ).argmax("seqlen")
-
-#     span_start_indices = best_spans / passage_length
-#     span_end_indices = ntorch.fmod(
-#         best_spans, passage_length
-#     )
-
-#     return span_start_indices, span_end_indices
 
 def tokenlist_to_passage(token_text):
     str_list = list(map(lambda x : x[2:] if len(x)>2 and x[:2]=="##" else " " + x, token_text))
